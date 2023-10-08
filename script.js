2a95930ae79e235edf2e9b39c7a32b3508e357c3
Chneemann <Andre-Ke@gmx.de>
2023-10-08 11:05:09 +0200

    Changes to be committed: modified:   header.html modified:   kontakt.html modified:   landingpage.html modified:   script.js modified:   send_mail.html modified:   style.css

--- script.js
+++ script.js
@@ -48,3 +48,13 @@ function sendMail(event) {
       console.log(error);
     });
 }
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
