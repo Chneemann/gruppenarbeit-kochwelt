8a72d298928988eee316dbc522399baa52dbf5a6
Chneemann <Andre-Ke@gmx.de>
2023-10-12 09:23:05 +0200

    Changes to be committed: modified:   .DS_Store renamed:    rezept.html -> daily-recipe.html modified:   header.html modified:   img/.DS_Store new file:   img/big-mac-salat.jpg new file:   img/christian.jpg modified:   index.html modified:   landingpage.html new file:   recipe-bigmacsalat.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -65,7 +65,69 @@ function closeNav() {
 
 /*  ------------------ RECIPEPAGE LOAD INGREDIENTS  ------------------ */
 
-function loadRecipe() {
+function loadRecipeBigMac() {
+  let recipeAmount = [
+    2.4, 0.4, 0.4, 2.4, 200, 200, 0, 0.2, 0.2, 2.4, 3.2, 0.8, 2.4, 0.8, 0.8,
+    0.4, 0.8,
+  ];
+  let recipeIngredients = [
+    "Hamburgerbrötchen mit Sesam",
+    "kleiner	Eisbergsalat",
+    "Pck	Schmelkäsescheibe(n) (Chester)",
+    "Gewürzgurke(n)",
+    "g Hackfleisch gemischt",
+    "g Rinderhackfleisch",
+    "Salz und Pfeffer",
+    "Glas	Salatcreme (Miracel Whip)",
+    "Tube(n)	Mayonnaise",
+    "EL	French Dressing mit wenig Dill",
+    "EL	Gewürgurke(n), fein gewürfelt",
+    "TL	Zucker",
+    "EL	Zwiebel(n), getrocknete, fein gewürfelt",
+    "TL	Essig",
+    "TL	Tomatenketchup",
+    "TL	Salz",
+    "EL	Gurkenrelish, süs-sauer",
+  ];
+
+  let amount = document.getElementById("recipepage-amount").value;
+
+  if (amount >= 1 && amount <= 12) {
+    document.getElementById("recipepage-load-ingredients-salad").innerHTML = "";
+    document.getElementById("recipepage-load-ingredients-sauce").innerHTML = "";
+
+    for (let count = 0; count < recipeAmount.length; count++) {
+      let recipeAmountNew = (recipeAmount[count] / 4) * amount;
+      let recipeAmountDecimal = divideDecimal(recipeAmountNew);
+
+      if (count < 7) {
+        document.getElementById(
+          "recipepage-load-ingredients-salad"
+        ).innerHTML += `
+        <tr>
+          <td>${recipeAmountDecimal} ${recipeIngredients[count]}</td>
+        </tr>`;
+      } else {
+        document.getElementById(
+          "recipepage-load-ingredients-sauce"
+        ).innerHTML += `
+      <tr>
+        <td>${recipeAmountDecimal} ${recipeIngredients[count]}  ${recipeAmountNew}</td>
+      </tr>`;
+      }
+    }
+    let element = document.getElementById("recipepage-amount");
+    element.style.backgroundColor = "var(--white)";
+    element.style.borderColor = "var(--green)";
+  } else {
+    let element = document.getElementById("recipepage-amount");
+    element.style.backgroundColor = "var(--red)";
+    element.style.borderColor = "var(--red)";
+    alert("Bitte eine Zahl zwischen 1 und 12 eingeben.");
+  }
+}
+
+function loadDailyRecipe() {
   let recipeAmount = [1, 6, 1, 1, 3, 1, 1, 2, 5, 1, 2, 2];
   let recipeIngredients = [
     "Kopfsalat(e)",
@@ -105,47 +167,48 @@ function loadRecipe() {
     element.style.borderColor = "var(--red)";
     alert("Bitte eine Zahl zwischen 1 und 12 eingeben.");
   }
+}
 
-  //------------- Umrechnung auf den Bruch und diesen Anzeigen lassen -------------
-  function divideDecimal(decimal) {
-    // Schritt 1: Ermitteln der ganzen Zahlkomponente
-    const wholeNumber = Math.floor(decimal);
+//------------- Umrechnung auf den Bruch und diesen Anzeigen lassen -------------
+function divideDecimal(decimal) {
+  // Schritt 1: Ermitteln der ganzen Zahlkomponente
+  const wholeNumber = Math.floor(decimal);
 
-    // Schritt 2: Ermitteln der Nachkommastellenkomponente
-    const fractionalPart = decimal - wholeNumber;
+  // Schritt 2: Ermitteln der Nachkommastellenkomponente
+  const fractionalPart = decimal - wholeNumber;
 
-    // Schritt 3: Initialisieren der Fraktionszeichenkette
-    let fraction = "";
+  // Schritt 3: Initialisieren der Fraktionszeichenkette
+  let fraction = "";
 
-    // Schritt 4: Überprüfen, ob die Dezimalzahl Nachkommastellen hat
-    if (decimal % 1 !== 0) {
-      // Schritt 5: Bestimmen der Fraktionskomponente basierend auf fractionalPart
-      if (fractionalPart <= 0.25) {
-        fraction = "&#xbc;"; // "&#xbc;" entspricht dem Bruch "¼"
-      } else if (fractionalPart <= 0.5) {
-        fraction = "&#xBD;"; // "&#xBD;" entspricht dem Bruch "½"
-      } else if (fractionalPart <= 0.75) {
-        fraction = "&#xbe;"; // "&#xbe;" entspricht dem Bruch "¾"
-      }
+  // Schritt 4: Überprüfen, ob die Dezimalzahl Nachkommastellen hat
+  if (decimal % 1 !== 0) {
+    // Schritt 5: Bestimmen der Fraktionskomponente basierend auf fractionalPart
+    const fractionValue = Math.round(fractionalPart * 4);
+    if (fractionValue === 1) {
+      fraction = "&#xbc;"; // "&#xbc;" entspricht dem Bruch "¼"
+    } else if (fractionValue === 2) {
+      fraction = "&#xBD;"; // "&#xBD;" entspricht dem Bruch "½"
+    } else if (fractionValue === 3) {
+      fraction = "&#xbe;"; // "&#xbe;" entspricht dem Bruch "¾"
     }
+  }
 
-    // Schritt 6: Überprüfen, ob es keine ganze Zahl gibt
-    if (wholeNumber === 0) {
-      // In diesem Fall wird nur die Fraktion zurückgegeben
-      return fraction;
-    }
+  // Schritt 6: Überprüfen, ob es keine ganze Zahl gibt
+  if (wholeNumber === 0) {
+    // In diesem Fall wird nur die Fraktion zurückgegeben
+    return fraction;
+  }
 
-    // Schritt 7: Überprüfen, ob es sowohl eine ganze Zahl als auch eine Fraktion gibt
-    else if (fraction) {
-      // In diesem Fall werden ganze Zahl und Fraktion kombiniert
-      // und als Zeichenkette zurückgegeben, getrennt durch ein Leerzeichen
-      return `${wholeNumber} ${fraction}`;
-    }
+  // Schritt 7: Überprüfen, ob es sowohl eine ganze Zahl als auch eine Fraktion gibt
+  else if (fraction) {
+    // In diesem Fall werden ganze Zahl und Fraktion kombiniert
+    // und als Zeichenkette zurückgegeben, getrennt durch ein Leerzeichen
+    return `${wholeNumber} ${fraction}`;
+  }
 
-    // Schritt 8: Wenn weder ganze Zahl noch Fraktion vorhanden sind
-    else {
-      // Die ganze Zahl wird in eine Zeichenkette umgewandelt und zurückgegeben
-      return wholeNumber.toString();
-    }
+  // Schritt 8: Wenn weder ganze Zahl noch Fraktion vorhanden sind
+  else {
+    // Die ganze Zahl wird in eine Zeichenkette umgewandelt und zurückgegeben
+    return wholeNumber.toString();
   }
 }
